8c6a92db0279b3b0eaf8b8e1e1791497d6cee202
Samuel Kim <samjkim455@gmail.com>
2025-06-02 13:55:42 -0400

    more done

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
-const state = [];
+const state = {
+  eventNames: [],
+  eventDetails: []
+};
+
+const entirePage = document.querySelector('#app');
+entirePage.innerHTML = `
+  <h1>Party Planner</h1>
+  <h2>Upcoming Parties</h2>
+`;
 
 const getApi = async () => {
   try {
     const res = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/events');
-    const actualResponse = await res.json();
-    const partyArray = actualResponse.data;
-    state.push(...partyArray);
+    const resJson = await res.json();
+    const partyArray = resJson.data;
+    puttingNamesInState(partyArray);
     render();
   }
   catch {
     console.log('Not Found');
   }
 }
-getApi();
 
-const eventParties = (arr) => {
-  let listofParties = "";
-  arr.forEach(elem => {
-    listofParties += `<output>${elem.name}</output>`;
+const puttingNamesInState = (arr) => {
+  arr.forEach(partyObj => {
+    state.eventNames.push(partyObj.name)
   });
-  return listofParties;
 }
+const detailsRender = async () => {
+  const response = await fetch();
+  const resJson = await response.json();
+  const partyDetailsArr = resJson.data;
 
-const partyDetails = () => {
-  const details = "";
-  for (let i = 0; i < state.length; i++) {
-    console.log('b');
-  }
-  return details;
+  const detailContainer = document.createElement('section');
+  detailContainer.innerText = state.eventDetails;
+  entirePage.appendChild(detailContainer);
+
+  state.eventDetails =;
 }
 
 const render = () => {
-  const entirePage = document.querySelector('#app');
-  entirePage.innerHTML = `
-    <h1>Party Planner</h1>
-    <h2>Upcoming Parties</h2>
-    <section>${eventParties(state)}</section >
-  `;
+  const ol = document.createElement('ol');
+  entirePage.appendChild(ol);
+
+  state.eventNames.forEach(partyName => {
+    const li = document.createElement('li');
+    li.addEventListener('click', () => {
+      detailsRender();
+    });
+    li.innerText = partyName;
+    ol.appendChild(li);
+  });
+
 }
+
+getApi();
